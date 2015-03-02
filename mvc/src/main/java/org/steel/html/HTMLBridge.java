0ce1b94b55f99f0a7cb2f4eedda4db1a88e6c68e
Alexandru Craciun <ax.craciun@gmail.com>
2015-03-02 21:26:05 +0100

    fix dyn expr example

--- mvc/src/main/java/org/steel/html/HTMLBridge.java
+++ mvc/src/main/java/org/steel/html/HTMLBridge.java
@@ -1,11 +1,10 @@
 package org.steel.html;
 
 import org.steel.model.DynamicExpr;
-import org.stjs.javascript.annotation.STJSBridge;
-import org.stjs.javascript.annotation.Template;
 
-@STJSBridge
 public class HTMLBridge {
-	@Template("assert")
-	public static native DynamicExpr expr(Object expr);
+	// @Template("assert")
+	public static DynamicExpr expr(Object expr) {
+		return null;
+	}
 }
--- mvc/src/main/java/org/steel/model/DynExpr.java
+++ mvc/src/main/java/org/steel/model/DynExpr.java
@@ -1,23 +1,24 @@
 package org.steel.model;
 
-import static org.stjs.javascript.Global.console;
 import static org.stjs.javascript.JSCollections.$map;
 
 import org.stjs.javascript.Map;
-import org.stjs.javascript.annotation.Template;
 import org.stjs.javascript.functions.Callback1;
+import org.stjs.javascript.functions.Callback3;
 import org.stjs.javascript.functions.Function0;
 
 public class DynExpr<T> {
 	private final Function0<T> expr;
 	private Callback1<DynExpr<T>> observer;
-	private final Map<String, Object> tracedFields;
-	private final Map<String, Object> tracedModels;
+	private Map<String, Object> tracedFields;
+	private Map<String, Object> tracedModels;
+	private Callback3<Object, String, Object> modelModifiedCallback;
 
 	private DynExpr(Function0<T> expr) {
 		this.expr = expr;
 		tracedFields = $map();
 		tracedModels = $map();
+		modelModifiedCallback = this::onModelModified;
 	}
 
 	public static <T> DynExpr<T> of(Function0<T> expr) {
@@ -25,6 +26,11 @@ public class DynExpr<T> {
 	}
 
 	public T value() {
+		for (String key : tracedModels) {
+			Observables.unobserve(tracedModels.$get(key), modelModifiedCallback);
+		}
+		tracedFields = $map();
+		tracedModels = $map();
 		return Observables.trace(this::trace, expr);
 	}
 
@@ -34,7 +40,7 @@ public class DynExpr<T> {
 			return;
 		}
 		if (tracedModels.$get(Observables.id(model)) == null) {
-			Observables.observe(model, this::onModelModified);
+			Observables.observe(model, modelModifiedCallback);
 			tracedModels.$put(Observables.id(model), model);
 		}
 		tracedFields.$put(key, model);
@@ -45,34 +51,14 @@ public class DynExpr<T> {
 		if (tracedFields.$get(key) == null) {
 			return;
 		}
-		//TODO accumulate changes and call async
+		// TODO accumulate changes and call async
 		observer.$invoke(this);
 	}
 
 	public void observe(Callback1<DynExpr<T>> callback) {
 		value();
-		//TODO multiple observers !?
+		// TODO multiple observers !?
 		this.observer = callback;
 	}
 
-	public static class MyModel {
-		@Template("property")
-		public String firstName;
-		@Template("property")
-		public String lastName;
-		@Template("property")
-		public String title;
-
-		public String name() {
-			return firstName + " " + lastName;
-		}
-	}
-
-	public static void main(String[] args) {
-		MyModel model = Observables.model(new MyModel());
-
-		DynExpr.of(() -> model.name()).observe((expr) -> console.info("NEW value:" + expr.value()));
-
-		model.firstName = "a";
-	}
 }
